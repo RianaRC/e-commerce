992309d4c7fd304af6da40af8b09c9e62f5f8639
RianaRC <riana@sobantu.co.za>
2018-05-28 12:13:55 +0300

    commit du branche Back

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @cat = Item.find(1)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Item1 = Item.create(title: 'cat1', description: 'so cute', price: 1.4, image_url: 'cat1.jpg' )
+Item1.save
+
+Item2 = Item.create(title: 'cat2', description: 'look at his eyes', price: 2, image_url: 'cat2.jpg' )
+Item2.save
+
+Item3 = Item.create(title: 'cat3', description: 'hahaha, lovely', price: 0.78, image_url: 'cat3.jpg' )
+Item3.save
+
+Item4 = Item.create(title: 'cat4', description: 'nobody can hate this little cat', price: 3.78, image_url: 'cat4.jpg' )
+Item4.save
+
+Item5 = Item.create(title: 'cat5', description: 'plus que mignon', price: 2.2, image_url: 'cat5.jpg' )
+Item5.save
+
+Item6 = Item.create(title: 'cat6', description: 'trés special ce chatton', price: 1.9, image_url: 'cat6.jpg' )
+Item6.save
+
+Item7 = Item.create(title: 'cat7', description: 'ohhh, fais attention a celui-ci', price: 2.7, image_url: 'cat7.jpg' )
+Item7.save
+
+Item8 = Item.create(title: 'cat8', description: 'Hola, regarde sa tete ', price: 1.8, image_url: 'cat8.jpg' )
+Item8.save
+
+Item9 = Item.create(title: 'cat9', description: 'hahaha, charmeur', price: 2, image_url: 'cat9.jpg' )
+Item9.save
+
+Item10 = Item.create(title: 'cat10', description: 'tu ne peux que tomber amoureux de celui-ci', price: 3.1, image_url: 'cat10.jpg' )
+Item10.save
+
+Item11 = Item.create(title: 'cat11', description: 'un peu de serieux dans le public', price: 1.28, image_url: 'cat11.jpg' )
+Item11.save
+
+Item12 = Item.create(title: 'cat12', description: 'Wawww, ', price: 0.6, image_url: 'cat12.jpg' )
+Item12.save
+
+Item13 = Item.create(title: 'cat13', description: 'groupe de chat trop mignons', price: 1.3, image_url: 'cat13.jpg' )
+Item13.save
+
+Item14 = Item.create(title: 'cat14', description: 'que le meilleur gagne', price: 1.9, image_url: 'cat14.jpg' )
+Item14.save
+
+Item15 = Item.create(title: 'cat15', description: 'la chaire de poule', price: 2, image_url: 'cat15.jpg' )
+Item15.save
+
+Item16 = Item.create(title: 'cat16', description: 'viens dormir please', price: 2.03, image_url: 'cat16.jpg' )
+Item16.save
+
+Item17 = Item.create(title: 'cat17', description: 'Trop mimi, right?? ', price: 0.78, image_url: 'cat17.jpg' )
+Item17.save
+
+Item18 = Item.create(title: 'cat18', description: 'le parreusseux', price: 1.1, image_url: 'cat18.jpg' )
+Item18.save
+
+Item19 = Item.create(title: 'cat19', description: 'ils jouent la, haha, super planque', price: 0.99, image_url: 'cat19.jpg' )
+Item19.save
+
+Item20 = Item.create(title: 'cat20', description: 'regarde ma chaussure, pointure 40', price: 4, image_url: 'cat20.jpg' )
+Item20.save
