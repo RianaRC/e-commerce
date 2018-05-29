a3ca9b132f3ce2eb4189a84336631e27587ddcae
Itokiana <itokianarajoelison@gmail.com>
2018-05-29 16:47:29 +0300

    Implement Cart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,18 @@ class HomeController < ApplicationController
   def show
   	@cat = Item.find(params[:id])
   end
+
+  def cart
+  	if user_signed_in?
+  		@cart = Cart.find_by(user_id: current_user.id)
+  		associations = AssociateItemCart.where(cart_id: @cart.id)
+  		@items = []
+  		associations.each do |a|
+  			@items.push(Item.find(a.item_id))
+  		end
+  		render "cart"
+  	else
+  		redirect_to(user_session_path)
+  	end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	
   root to: 'home#index'
   get "/items/:id", to: 'home#show', as: "item_show"
+  get "/cart", to: 'home#cart', as: "cart_show"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
