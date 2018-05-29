815c9517d2fa2f9da837d89e31068a393c5b6c85
RianaRC <riana@sobantu.co.za>
2018-05-29 15:50:15 +0300

    hihi

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_093103) do
+ActiveRecord::Schema.define(version: 2018_06_29_111323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "associate_item_carts", force: :cascade do |t|
-    t.bigint "item_id"
-    t.bigint "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_associate_item_carts_on_cart_id"
-    t.index ["item_id"], name: "index_associate_item_carts_on_item_id"
-  end
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id"
@@ -38,6 +29,13 @@ ActiveRecord::Schema.define(version: 2018_05_29_093103) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,7 +55,6 @@ ActiveRecord::Schema.define(version: 2018_05_29_093103) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "associate_item_carts", "carts"
-  add_foreign_key "associate_item_carts", "items"
   add_foreign_key "carts", "users"
+  add_foreign_key "orders", "users"
 end
