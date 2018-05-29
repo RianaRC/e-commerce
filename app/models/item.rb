40b288202e426fbcd2f17969dbc599fa2ac702f2
Itokiana <itokianarajoelison@gmail.com>
2018-05-29 14:42:38 +0300

    Create table orders

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
-	has_many :associate_item_cart
-	has_many :cart, through: :order
+	has_many :associate_item_carts
+	has_many :cart, through: :associate_item_carts
+
+	has_many :associate_order_items
+	has_many :order, through: :associate_order_items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_093103) do
+ActiveRecord::Schema.define(version: 2018_05_29_111323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2018_05_29_093103) do
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_associate_item_carts_on_cart_id"
     t.index ["item_id"], name: "index_associate_item_carts_on_item_id"
+  end
+
+  create_table "associate_order_items", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_associate_order_items_on_item_id"
+    t.index ["order_id"], name: "index_associate_order_items_on_order_id"
   end
 
   create_table "carts", force: :cascade do |t|
@@ -38,6 +47,13 @@ ActiveRecord::Schema.define(version: 2018_05_29_093103) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,5 +75,8 @@ ActiveRecord::Schema.define(version: 2018_05_29_093103) do
 
   add_foreign_key "associate_item_carts", "carts"
   add_foreign_key "associate_item_carts", "items"
+  add_foreign_key "associate_order_items", "items"
+  add_foreign_key "associate_order_items", "orders"
   add_foreign_key "carts", "users"
+  add_foreign_key "orders", "users"
 end
