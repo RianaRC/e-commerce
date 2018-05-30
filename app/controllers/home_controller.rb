10a68381166c78b2b070e556d3f64cc3bbe8dc88
Itokiana <itokianarajoelison@gmail.com>
2018-05-30 15:17:07 +0300

    Modif HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,17 @@ class HomeController < ApplicationController
     @cats = Item.all
     if user_signed_in?
     	@cart = Cart.find_by(user_id: current_user.id)
-  		associations = AssociateItemCart.where(cart_id: @cart.id)
-  		@items = []
-  		associations.each do |a|
-  			@items.push(Item.find(a.item_id))
-  		end
+    	if @cart != nil
+	  		associations = AssociateItemCart.where(cart_id: @cart.id)
+	  		@items = []
+	  		associations.each do |a|
+	  			@items.push(Item.find(a.item_id))
+	  		end
+    	else
+    		@cart = Cart.new
+    		@cart.user = current_user
+    		@items = []
+    	end
     end
   end
 
@@ -31,7 +37,19 @@ class HomeController < ApplicationController
   		associations.each do |a|
   			@items.push(Item.find(a.item_id))
   		end
-  		render "cart"
+  		if @cart != nil
+	  		associations = AssociateItemCart.where(cart_id: @cart.id)
+	  		@items = []
+	  		associations.each do |a|
+	  			@items.push(Item.find(a.item_id))
+	  		end
+  			render "cart"
+    	else
+    		@cart = Cart.new
+    		@cart.user = current_user
+    		@items = []
+    		render "cart"
+    	end
   	else
   		redirect_to(user_session_path)
   	end
