f6eac6e4ae00582d0032531a196faa93decc15ec
Itokiana <itokianarajoelison@gmail.com>
2018-06-01 02:58:03 +0300

    Final 1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -53,6 +53,20 @@ class HomeController < ApplicationController
   end
 
   def profil
-	@items = Item.all
+    if user_signed_in?
+      @cart = Cart.find_by(user_id: current_user.id)
+      if @cart != nil
+        associations = AssociateItemCart.where(cart_id: @cart.id)
+        @items = []
+        associations.each do |a|
+          @items.push(Item.find(a.item_id))
+        end
+      else
+        @cart = Cart.new
+        current_user.cart = @cart
+        @cart.save
+        @items = []
+      end
+    end
   end
 end
